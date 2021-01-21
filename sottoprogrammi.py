e2eb4b8812aaca7fefdc8a5a0cec0c54b11bfd89
ddn33 <74098425+ddn33@users.noreply.github.com>
2021-01-21 16:35:18 +0100

    effettuate piccole modifiche ai file main.py e sottoprogrammi.py

--- sottoprogrammi.py
+++ sottoprogrammi.py
@@ -13,15 +13,15 @@ def pgn_parser(filename):
     
     game = chess.pgn.read_game(pgn)
     node = game
-    positions = []
     moves = []
     # All positions and moves until the game ends
     while not node.is_end():
         nextNode = node.variation(0)
         move = node.board().san(nextNode.move)
-        position = nextNode.board().fen()
         moves.append(move)
-        positions.append(position)
         node = nextNode
         
     return moves
+
+
+
