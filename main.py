2edba0251c79531d674c7df6fe289cb38c9b3fe5
camardanic <geom.camarda@gmail.com>
2021-01-21 15:50:02 +0100

    Creato il file main per interfaccia utente

---
+++ main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 21 12:59:15 2021
+
+@author: geomc
+"""
+
+
+from lettura import pgn_parser
+
+FILENAME = ('Partita-9060.pgn')
+
+data = pgn_parser(FILENAME)
+
