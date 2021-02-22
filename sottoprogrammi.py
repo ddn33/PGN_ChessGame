939b5fd2e0a5c160dc42f400ab0e4de0599668cd
ddn33 <74098425+ddn33@users.noreply.github.com>
2021-02-22 13:40:38 +0100

    Modificato il pgn_parser in modo da restituire la lista delle mosse e non il dataframe

--- sottoprogrammi.py
+++ sottoprogrammi.py
@@ -24,7 +24,33 @@ def pgn_parser(filename):
         list_df = pd.DataFrame(moves)
         list_df = list_df.rename(columns= {0: 'mosse'} )
                 
-    return list_df
+    return list(list_df['mosse'])
+
+
+def get_features_mosse(lista_mosse):
+    """
+    IL SEGUENTE DATAFRAME CONTIENE TUTTE LE INFORMAZIONI NECESSARIE PER IDENTIFICARE
+    UNIVOCAMENTE LA MOSSA RELATIVA AD UN PEZZO SULLA SCACCHIERA, OGNI COLONNA è RICAVATA
+    ANALIZZANDO LA LISTA DELLE MOSSE ESTRATTA DAL FILE PGN
+    """
+    mossa_feature = []
+    for i in range(len(lista_mosse)):
+        mossa_feature[i] = {'pezzo':'0','casa':0,'cattura':False, 'arrocco_corto':False, 'arrocco_lungo':False, 'promozione':False,'pezzo_promozione': None,}
+    
+    for mossa in lista_mosse:
+        for ch in mossa:
+            if ch.isupper():
+                mossa_feature[mossa]['pezzo'] = ch
+    return mossa_feature
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 
 
