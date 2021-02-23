2ce24e42cf71964eee72efe732eb4f65bca63053
ddn33 <74098425+ddn33@users.noreply.github.com>
2021-02-23 15:28:31 +0100

    aggiunto al fle sottoprogrammi.py metodo per la trasformazione del nome di una casa nela posizione su un array8x8

--- sottoprogrammi.py
+++ sottoprogrammi.py
@@ -33,19 +33,47 @@ def get_features_mosse(lista_mosse):
     UNIVOCAMENTE LA MOSSA RELATIVA AD UN PEZZO SULLA SCACCHIERA, OGNI COLONNA è RICAVATA
     ANALIZZANDO LA LISTA DELLE MOSSE ESTRATTA DAL FILE PGN
     """
+    
+    "creazione lista delle informazioni necessarie per ogni mossa da effettuare"
     mossa_feature = []
     for i in range(len(lista_mosse)):
-        mossa_feature[i] = {'pezzo':'0','casa':0,'cattura':False, 'arrocco_corto':False, 'arrocco_lungo':False, 'promozione':False,'pezzo_promozione': None,}
+        mossa_feature.append({'pezzo':'0','colore':0,'casa':0,'cattura':False, 'arrocco_corto':False, 'arrocco_lungo':False, 'promozione':False,'pezzo_promozione': None})
     
+    
+    "parte 1: identificazione del pezzo da muovere n.b. indici pari bianco, indici dispari nero"
+    count = 0
     for mossa in lista_mosse:
         for ch in mossa:
             if ch.isupper():
-                mossa_feature[mossa]['pezzo'] = ch
+                mossa_feature[count]['pezzo'] = ch
+        count +=1
+    
+    
+    "parte 2: identificazione della casa verso la quale deve muoversi il pezzo"
+    
+    
+    
     return mossa_feature
     
     
     
     
+    
+
+
+def get_posizione_casa(casa):
+    posizione = [0, 0]
+    temp = []
+    for ch in casa:
+        temp.append(ch)
+    file = temp[0]  # Lettera sulla scacchiera (colonna)
+    rank = temp[1]  # Numero sulla scacchiera (fila)
+
+    posizione[0] = ord(file) - ord('a')
+    posizione[1] = int(rank) - 1
+
+    return tuple(posizione)
+
     
     
     
