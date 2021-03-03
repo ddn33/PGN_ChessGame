cd3b9424cb60c4fe6bdb3114036166ecfa3e406f
camardanic <geom.camarda@gmail.com>
2021-03-03 18:14:41 +0100

    Implementato il metodo arrocco della classe mossa, utilizzato dalla classe partita nel metodo muovi

--- partita.py
+++ partita.py
@@ -1,5 +1,7 @@
 import scacchiera
 from Arbitro import arbitro
+from mossa import mossa
+
 """
 Classe che contiene i comandi utilizzabili dal main per il proseguimento della partita e 
 il controllo dello stato dei pezzi sulla scacchiera relativa ad una determinata partita
@@ -18,24 +20,32 @@ class partita():
         self.arbitro = arbitro(self,self.scacchiera)
 
     def muovi(self,mossa):
-        if mossa.cattura == True:
-            if self.arbitro.cattura_valida(mossa) == True:
+        if mossa.arrocco_corto == True or mossa.arrocco_lungo == True:
+            mossa.arrocco(self.scacchiera,mossa)
+            
+        elif mossa.cattura == True:
+            if self.arbitro.cattura_valida(mossa):
                 pezzo_da_muovere = self.scacchiera.quale_pezzo_si_muove(mossa)
+                
                 for pezzo in self.scacchiera.pezzi:
                     if pezzo.casa == mossa.casa:
                         self.scacchiera.pezzi.remove(pezzo)
                         # break                
                 pezzo_da_muovere.casa = mossa.casa
-                self.scacchiera.aggiorna_scacchiera()
                 
             else:
                 print("MOSSA NON VALIDA")
         else:    
             if self.arbitro.mossa_valida(mossa):
-                "MUOVI"
+                pezzo_da_muovere = self.scacchiera.quale_pezzo_si_muove(mossa)
+                pezzo_da_muovere.casa = mossa.casa
             else:
                 "MOSSA NON VALIDA"
         
+        
+        
+        
+        self.scacchiera.aggiorna_scacchiera()
             
         
 
