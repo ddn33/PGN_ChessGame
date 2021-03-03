8009b5054a18eb2807e0659c8bf6f627dbfa26f0
ddn33 <74098425+ddn33@users.noreply.github.com>
2021-03-03 14:25:16 +0100

    implementati anche per la torre i metodi puoi andare in() e lista mosse possibili torre()

--- torre.py
+++ torre.py
@@ -9,5 +9,73 @@ class torre(pezzo):
         self.nome = 'R'
         super().__init__(colore, casa, scacchiera)
 
-    def puoi_andare_in(self):
-        pass
+    def puoi_andare_in(self,mossa):
+                
+        mosse_possibili = self.lista_mosse_possibili_torre()
+        
+        if mossa.casa in mosse_possibili:
+            return True
+        else:
+            return False
+        
+    def lista_mosse_possibili_torre(self):
+        mosse_possibili = []
+        casa_attuale = self.scacchiera.get_posizione_casa(self.casa)
+        
+        """
+        la costruzione delle mosse possibili della torre è uguale a quella 
+        dell'alfiere, l'unica differenza è la direzione di movimento
+        coerentemente con le regole del gioco è parallela agli assi della 
+        scacchiera (e non in diagonale)
+        """
+        
+        stop = False
+        i = 1
+        while stop == False:
+            if stop == False and casa_attuale[0]+i<=7:
+                mosse_possibili.append((casa_attuale[0]+i,casa_attuale[1]))
+            else:
+                stop = True
+            if self.scacchiera.is_free_la_casella((casa_attuale[0]+i,casa_attuale[1])) == False:
+                stop = True
+            i = i+1
+            
+        stop = False
+        i = 1
+        while stop == False:
+            if stop == False and casa_attuale[0]-i>=0:
+                mosse_possibili.append((casa_attuale[0]-i,casa_attuale[1]))
+            else:
+                stop = True
+            if self.scacchiera.is_free_la_casella((casa_attuale[0]-i,casa_attuale[1])) == False:
+                stop = True
+            i = i+1
+            
+        stop = False
+        i = 1
+        while stop == False:
+            if stop == False and casa_attuale[1]-i>=0:
+                mosse_possibili.append((casa_attuale[0],casa_attuale[1]-i))
+            else:
+                stop = True
+            if self.scacchiera.is_free_la_casella((casa_attuale[0],casa_attuale[1]-i)) == False:
+                stop = True
+            i = i+1
+            
+        stop = False
+        i = 1
+        while stop == False:
+            if stop == False and casa_attuale[1]+i<=7:
+                mosse_possibili.append((casa_attuale[0],casa_attuale[1]+i))
+            else:
+                stop = True
+            if self.scacchiera.is_free_la_casella((casa_attuale[0],casa_attuale[1]+i)) == False:
+                stop = True
+            i = i+1
+                 
+        case_possibili = []
+        for i in range(len(mosse_possibili)):
+            case_possibili.append(self.scacchiera.get_casa(tuple(mosse_possibili[i])))
+      
+            
+        return case_possibili
