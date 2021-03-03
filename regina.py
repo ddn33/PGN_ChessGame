e609b1cdd4391becac41da18319d899956a3a576
ddn33 <74098425+ddn33@users.noreply.github.com>
2021-03-03 14:26:28 +0100

    implementati anche per la regina i metodi puoi andare in() e lista mosse possibili regina()

--- regina.py
+++ regina.py
@@ -1,4 +1,6 @@
 from pezzi import pezzo, color
+from alfiere import alfiere
+from torre import torre
 
 class regina(pezzo):
     def __init__(self, colore, casa, scacchiera):
@@ -9,5 +11,29 @@ class regina(pezzo):
         self.nome = 'Q'
         super().__init__(colore, casa, scacchiera)
 
-    def puoi_andare_in(self):
-        pass
+    def puoi_andare_in(self,mossa):
+        mosse_possibili = self.lista_mosse_possibili_regina()
+        
+        if mossa.casa in mosse_possibili:
+            return True
+        else:
+            return False
+
+
+    def lista_mosse_possibili_regina(self):
+        
+        
+        """
+        le mosse che può effettuare la regina sono la somma delle mosse
+        che potrebbe effettuare un alfiere e quelle che potrebbe effettuare una
+        torre, quindi unendo i metodi lista_mosse_possibili relativi ad alfiere
+        e torre possiamo ricavare tutte le mosse che può effettuare una regina
+        """
+        
+        mosse_diagonali = alfiere.lista_mosse_possibili_alfiere(self)
+        mosse_parallele = torre.lista_mosse_possibili_torre(self)
+        case_possibili = mosse_diagonali+mosse_parallele
+
+        return case_possibili
+        
+        
